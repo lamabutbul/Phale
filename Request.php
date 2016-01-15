119c4c5567c641c49c4670241bbb73a0f4cd8cf9
Moshe Abutbul <lamabutbul@gmail.com>
2016-01-15 15:41:07 +0200

    Prepare request.

--- Request.php
+++ Request.php
@@ -5,4 +5,37 @@ namespace Phale;
 
 class Request {
 
+    /**
+     * @var string
+     */
+    public $protocol;
+
+    /**
+     * @var string
+     */
+    public $method;
+
+    /**
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Request constructor.
+     */
+    public function __construct() {
+        $this->protocol = $_SERVER['SERVER_PROTOCOL'];
+        $this->method = strtoupper($_SERVER['REQUEST_METHOD']);
+        $this->path = $this->getPath();
+    }
+
+    /**
+     * Get the path from string.
+     * @return string
+     */
+    private function getPath() {
+        $parts = explode('?', $_SERVER['REQUEST_URI']);
+        return $parts[0];
+    }
+
 }
